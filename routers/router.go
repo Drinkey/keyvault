0b170d3c30d43e1d702b30cba4aa4f2306325edf
Junkai Zhang <junkai.zhang@watchguard.com>
2020-12-11 10:51:37 +0800

    feat: adjust router mapping

--- routers/router.go
+++ routers/router.go
@@ -13,18 +13,30 @@ func InitRouter() *gin.Engine {
 	{
 		apiv1.GET("/ping", v1.Ping)
 		// only list namespace of vault
-		apiv1.GET("/namespace", v1.ListNamespace)
+		apiv1.GET("/namespace", v1.ListNamespaces)
 		apiv1.POST("/namespace", v1.CreateNamespace)
+		// TODO: only name can be updated
+		// apiv1.PUT("/namespace", v1.UpdateNamespace)
+		// TODO: need authorization, it would be cost operation, rekey ns would
+		// require all secrets with this ns updated.
+		// apiv1.POST("/namespace/rekey", v1.RekeyNamespace)
 		// respond to URL like /vault/gitlab/?q=k8s_password
-		apiv1.GET("/vault/:namespace", v1.GetSecrets)
-		apiv1.POST("/vault/:namespace", v1.CreateSecrets)
-		// TODO:
-		// v1.DELETE("/vault/:namespace", v1.secret.Delete)
-		// TODO:
-		// v1.PUT("/vault/:namespace", v1.secret.Update)
-		// another user story
-		// TODO:
-		// v1.POST("/cert/", v1.controller.SignCSR)
+		apiv1.GET("/vault/:namespace", v1.GetSecret)
+		apiv1.POST("/vault/:namespace", v1.CreateSecret)
+		// TODO: respond to URL like /vault/gitlab/?q=k8s_password
+		// apiv1.DELETE("/vault/:namespace", v1.DeleteSecret)
+		// TODO: only value can be updated, namespace and key can't
+		// v1.PUT("/vault/:namespace", v1.UpdateSecret)
+
+		// Certificate need extra works, auth or token
+		// TODO: we may need this API exposed in HTTP, not HTTPS
+		// v1.POST("/cert/req", v1.CreateCertificateRequest)
+		// v1.GET("/cert/ca", v1.GetCACertificate)
+		// TODO: OU should be unique
+		// v1.GET("/cert/", v1.GetCertificate)
+		// TODO: only limited user should be able to access this API, how
+		// v1.POST("/cert/issue", v1.CreateCertificateRequest)
+
 	}
 	return r
 }
