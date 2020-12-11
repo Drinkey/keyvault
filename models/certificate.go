4305f206a9dad27551ecd4a36db41452c14f5a0f
Junkai Zhang <junkai.zhang@watchguard.com>
2020-12-11 10:50:12 +0800

    feat: support certficate model

--- models/certificate.go
+++ models/certificate.go
@@ -3,7 +3,27 @@ package models
 type Certificate struct {
 	ID          uint   `gorm:"primaryKey;autoIncrement"`
 	Name        string `gorm:"unique;not null"`
-	SignRequest string
+	SignRequest string `gorm:"not null"`
 	Certificate string
 	Token       string `gorm:"unique;not null"`
+}
+
+func CreateCertificateRequest(name, req, token string) (err error) {
+	err = db.Create(&Certificate{
+		Name:        name,
+		SignRequest: req,
+		Token:       token,
+	}).Error
+	if err != nil {
+		return
+	}
+	return nil
+}
+
+func GetCertificate(name string) (cert Certificate, err error) {
+	err = db.Where("name = ?", name).First(&cert).Error
+	if err != nil {
+		return Certificate{}, err
+	}
+	return
 }
--- models/db.go
+++ models/db.go
@@ -1,12 +1,49 @@
 package models
 
 import (
+	"log"
 	"os"
 
+	"github.com/Drinkey/keyvault/internal"
 	_ "github.com/mattn/go-sqlite3"
+	"gorm.io/driver/sqlite"
 	"gorm.io/gorm"
 )
 
 var DBPATH string = os.Getenv("DB_PATH")
 
 var db *gorm.DB
+
+func init() {
+	log.SetPrefix("model - ")
+
+	dsn := DBPATH
+
+	log.Printf("initializing database: %s", DBPATH)
+
+	if dsn == "" {
+		log.Print("Specify the DB Path in environment variable DB_PATH")
+		log.Print("Without DB file, run in-memory")
+		dsn = "file::memory:?cache=shared"
+	}
+
+	initDbRequired := true
+
+	if internal.FileExist(DBPATH) {
+		log.Printf("database file %s already exist", DBPATH)
+		initDbRequired = false
+	}
+
+	connDB, err := gorm.Open(sqlite.Open(dsn), &gorm.Config{})
+	if err != nil {
+		log.Panic(err)
+	}
+	db = connDB
+
+	if initDbRequired {
+		log.Println("first install, initializing database schema")
+		db.AutoMigrate(&Secret{})
+		db.AutoMigrate(&Namespace{})
+		db.AutoMigrate(&Certificate{})
+	}
+}
