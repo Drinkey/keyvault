967c0d98baf076b52a456cec97cd311614c36063
Junkai Zhang <junkai.zhang@watchguard.com>
2020-12-21 09:20:11 +0800

    test: use defer to clear env

--- internal/utils_test.go
+++ internal/utils_test.go
@@ -8,22 +8,22 @@ import (
 func TestFileExistDirReturnsFalse(t *testing.T) {
 	f := "/tmp/somedir"
 	os.Mkdir(f, 0777)
+	defer os.Remove(f)
 	r := FileExist(f)
 	if r {
 		t.Log("expect return false but got true")
 		t.Fail()
 	}
-	os.Remove(f)
 }
 
 func TestFileExistFileReturnsTrue(t *testing.T) {
 	f := "/tmp/somefile"
 	// file := os.File{}
 	os.Create(f)
+	defer os.Remove(f)
 	r := FileExist(f)
 	if !r {
 		t.Log("expect return false but got true")
 		t.Fail()
 	}
-	os.Remove(f)
 }
